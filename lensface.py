6dd62091dad19f04e073f90d5b1343bcfb89d4b7
Matthew Withers <matthew.withers@port.ac.uk>
2017-05-23 00:13:28 +0100

    Fixed alot of physics

--- lensface.py
+++ lensface.py
@@ -39,14 +39,14 @@ def make_gauss(centre, amp, sig, shapdim):
 
     iter = 0
     radius = 0
-    while (iter < sidelenx) :
-        if (gaussblob[hy][hx+iter] < 0.1*gaussblob[hy][hx]) :
-            radius = iter
-            break
-        else :
-            iter += 1
+    # while (iter < sidelenx) :
+    #     if (gaussblob[hy][hx+iter] < gaussblob[hy][hx]) :
+    #         radius = iter
+    #         break
+    #     else :
+    #         iter += 1
 
-    return gaussblob, int(radius)
+    return -1.0*gaussblob, int(radius)
 
 
 def calc_tranform(grad, lenx, leny):
@@ -58,10 +58,91 @@ def calc_tranform(grad, lenx, leny):
 
     for i in range(0, lenx):
 
+        # print(i)
+
         for j in range(0, leny):
 
-            newxpos[index]=i-int(grad[1][i][j]) % lenx
-            newypos[index]=j-int(grad[0][i][j]) % leny
+            # print(i, j)
+
+            newxpos[index]=i-int(grad[0][i][j]) % lenx
+            newypos[index]=j-int(grad[1][i][j]) % leny
+
+            gradx = grad[0][i][j]
+            grady = grad[1][i][j]
+
+            ii = i-int(grad[0][i][j]) % lenx
+            jj = j-int(grad[1][i][j]) % leny
+            minx = 1000
+            while True:
+                if abs(grad[0][ii][jj]) < 1.0:
+                    break
+                new_minx = i - ii + int(grad[0][ii][jj])
+                delta = minx - new_minx
+                # print (ii, jj, delta)
+                if delta < 0:
+                    break
+                else:
+                    ii -= 1
+                    jj -= 1
+                    minx = new_minx
+
+            newxpos[index] = (i - int(grad[0][(ii+1)%lenx][(jj+1)%leny]))%lenx
+
+            ii = i-int(grad[0][i][j]) % lenx
+            jj = j-int(grad[1][i][j]) % leny
+            miny = 1000
+            while True:
+                if abs(grad[0][ii][jj]) < 1.0:
+                    break
+                new_miny = j - jj + int(grad[1][ii][jj])
+                delta = miny - new_miny
+                # print (ii, jj, delta)
+                if delta < 0:
+                    break
+                else:
+                    ii -= 1
+                    jj -= 1
+                    miny = new_miny
+
+            newypos[index] = (j - int(grad[1][(ii+1)%lenx][(jj+1)%leny]))%leny
+
+            # if grad[0][newxpos[index]][j] < gradx :
+            #     delta = abs(grad[0][newxpos[index]][j] - grad[0][(newxpos[index]-1)%lenx][j])
+            #     while grad[0][newxpos[index]][j] < gradx and delta > 1.0:
+            #         # print(gradx-grad[0][newxpos[index]][j])
+            #         # print(delta)
+            #         xind = (newxpos[index] - 1)
+            #         gradxx = int(grad[0][xind][j])
+            #         newxpos[index]=(i-gradxx)
+            #         delta = abs(grad[0][newxpos[index]][j] - grad[0][(newxpos[index]-1)%lenx][j])
+            #         print(xind)
+            #
+            # else :
+            #     delta = abs(grad[0][newxpos[index]][j] - grad[0][(newxpos[index]+1)%lenx][j])
+            #     while grad[0][newxpos[index]][j] > gradx and delta < -1.0:
+            #         xind = (newxpos[index] + 1) % lenx-1
+            #         gradxx = int(grad[0][xind][j])
+            #         newxpos[index]=(i-gradxx) % lenx-1
+            #         delta = abs(grad[0][newxpos[index]][j] - grad[0][(newxpos[index]+1)%lenx][j])
+            #
+            #
+            # if grad[1][i][newypos[index]] < grady :
+            #     delta = abs(grad[1][i][newypos[index]] - grad[1][i][(newypos[index]-1)%leny])
+            #     while grad[1][i][newypos[index]] < grady and delta > 1.0:
+            #         yind = (newypos[index] - 1) % leny-1
+            #         gradyy = grad[1][i][yind]
+            #         newypos[index]=(j-gradyy) % leny-1
+            #         delta = abs(grad[1][i][newypos[index]] - grad[1][i][(newypos[index]-1)%leny])
+            #         # print(yind, gradyy, newypos[index])
+            #
+            # else :
+            #     delta = abs(grad[1][i][newypos[index]] - grad[1][i][(newypos[index]+1)%leny])
+            #     while grad[1][i][newypos[index]] > grady and delta < -1.0:
+            #         yind = (newypos[index] + 1) % leny-1
+            #         gradyy = int(grad[1][i][yind])
+            #         newypos[index]=(j-gradyy) % leny-1
+            #         delta = abs(grad[1][i][newypos[index]] - grad[1][i][(newypos[index]+1)%leny])
+
             index+=1
 
     return (newxpos, newypos)
